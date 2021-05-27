5eb1fc3aa4b280c4ca65f93ee2e96ef76c5a5d07
y2k <itwisterlx@gmail.com>
2021-05-28 01:36:11 +0300

    refactoring

--- app/MsgSerializer.fs
+++ app/MsgSerializer.fs
@@ -2,25 +2,20 @@ module MyGetBot.MsgSerializer
 
 [<CLIMutable>]
 type t =
-    { id: int
-      tag: int
+    { tag: int
       user: string
       url: string }
-
-let private getId x = x.id
 
 let serialize =
     function
     | SyncRequested _ -> None
     | UrlAdded (User user, Url url) ->
-        { id = 0
-          tag = 1
+        { tag = 1
           user = user
           url = url }
         |> Some
     | UrlRemoved (User user, Url url) ->
-        { id = 0
-          tag = 2
+        { tag = 2
           user = user
           url = url }
         |> Some
@@ -30,5 +25,3 @@ let deserialize =
     | { tag = 1; user = user; url = url } -> UrlAdded(User user, Url url)
     | { tag = 2; user = user; url = url } -> UrlRemoved(User user, Url url)
     | value -> failwithf "Can't parse %O" value
-
-let info = getId, serialize, deserialize
